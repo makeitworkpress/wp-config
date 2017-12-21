a6cb1044b106b83222427e154de8d523c7f77b52
Michiel <michielsite@hotmail.com>
2017-12-21 16:53:25 +0100

    New folder structure for better composer compatibility

--- src/config.php
+++ src/config.php
@@ -4,7 +4,7 @@
  * It can load configurations from an array or a file
  * If the configurations are loaded from a file, the file itself should return these configurations
  */
-namespace WP_Config;
+namespace MakeitWorkPress\WP_Config;
 use WP_Error as WP_Error;
 
 defined( 'ABSPATH' ) or die( 'Go eat veggies!' );
